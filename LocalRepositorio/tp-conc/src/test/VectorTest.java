559199c0c5b5e848474e44d35c1b07db153d5c2f
ferblanco75 <ferblanco@gmail.com>
2018-10-22 23:19:42 -0300

    agrego clase para tests de vector concurrente

---
+++ LocalRepositorio/tp-conc/src/test/VectorTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class VectorTest {
+    @Test
+    public void testTamanioVector(){
+        Assert.assertTrue(true);
+    }
+
+}
