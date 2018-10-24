258c76b52f21ce36c69c26ec9792056d9086c1e0
ferblanco75 <ferblanco@gmail.com>
2018-10-24 16:21:32 -0300

    merge de los trabajos y correcciones

--- LocalRepositorio/tp-conc/src/tests/ThreadPoolTest.java
+++ LocalRepositorio/tp-conc/src/tests/ThreadPoolTest.java
@@ -1,69 +1,8 @@
-<<<<<<< HEAD
-package tests;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import progConc.*;
-
-public class ThreadPoolTest {
-
-    private ThreadPool tp;
-    private ConcurVector vector;
-    private ConcurVector vectorModificado;
-    private Operacion operacion;
-    private int dimension;
-    private int threads;
-    private  Thread [] workers;
-    private int rango;
-    private int resto;
-
-
-
-    @Before
-
-    public void setUp(){
-
-
-        this.tp = new ThreadPool();
-        this.vector =new ConcurVector(8,4);
-        this.vectorModificado= new ConcurVector(8,4);
-        this.operacion= new Add();
-        this.dimension= vectorModificado.dimension();
-        this.threads= vector.getThread();
-        this.workers = new Thread [this.threads];
-        this.rango= (this.dimension / this.threads);
-        this.resto= (this.dimension % this.threads);
-
-
-    }
-
-    @Test
-    //testeo que el threadpool se inicialize con una operacion suma y 2 vectores de diferentes dimensiones
-    public void testeoQueElThreadpoolInicialice(){
-        tp.initializeWorkers(operacion,vector,vectorModificado);
-
-        Assert.assertEquals(8, vectorModificado.dimension());
-
-    }
-    @Test
-    public void testeoQueElRangoDelThreadPoolSea2(){
-        tp.initializeWorkers(operacion,vector,vectorModificado);
-
-        Assert.assertEquals(2, tp.getRango());
-
-    }
-
-
-
-
-
-
-}
-=======
-package tests;
-
-import static org.junit.Assert.*;
-
+//package tests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -77,45 +16,47 @@ import progConc.Worker;
 
 import static org.mockito.Mockito.*;
 
-public class ThreadPoolTest {
-     
-	ThreadPool threadPool;
-	//Worker mockWorker; 
-	Operacion dummyOperacion;
-	Operacion operacion;
-    ConcurVector vector1, dummyVector;
-    Worker mockWorker;
-    
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
-	@Before
-	public void setUp() throws Exception {
-		 dummyOperacion =  mock(Operacion.class);
-	//	Operacion operacion= new Abs();
-		vector1= new ConcurVector(7,3);
-		dummyVector= mock (ConcurVector.class);
-		mockWorker = mock (Worker.class) ; 
-	    threadPool= new ThreadPool();
-	   
-	}
 
-	@After
-	public void tearDown() throws Exception {
-		
-		
-	}
+    public class ThreadPoolTest {
 
-	@Test
-	public void testInitializeWorkers() throws InterruptedException {
-		
-		threadPool.initializeWorkers(operacion,vector1,dummyVector);
-		// Verifica que se instanciaron 3 Threads workers
-		//verify(dummyOperacion, times(0)).operar(mockWorker);
-		verify(mockWorker, atLeast(1)).start();
-	}
+        ThreadPool threadPool;
+        //Worker mockWorker;
+        Operacion dummyOperacion;
+        Operacion operacion;
+        ConcurVector vector1, dummyVector;
+        Worker mockWorker;
 
-}
->>>>>>> b96590bff97449adf17633596ba6c7140c80f2d7
+
+        @BeforeClass
+        public static void setUpBeforeClass() throws Exception {
+        }
+
+        @Before
+        public void setUp() throws Exception {
+            dummyOperacion = mock(Operacion.class);
+            //	Operacion operacion= new Abs();
+            vector1 = new ConcurVector(7, 3);
+            dummyVector = mock(ConcurVector.class);
+            mockWorker = mock(Worker.class);
+            threadPool = new ThreadPool();
+
+        }
+
+        @After
+        public void tearDown() throws Exception {
+
+
+        }
+
+        @Test
+        public void testInitializeWorkers() throws InterruptedException {
+
+            threadPool.initializeWorkers(operacion, vector1, dummyVector);
+            // Verifica que se instanciaron 3 Threads workers
+            //verify(dummyOperacion, times(0)).operar(mockWorker);
+            verify(mockWorker, atLeast(1)).start();
+        }
+
+    }
+
--- LocalRepositorio/tp-conc/src/tests/WorkerTest.java
+++ LocalRepositorio/tp-conc/src/tests/WorkerTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package tests;
 
 import org.junit.After;
@@ -12,6 +11,10 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.atLeast;
+import progConc.ConcurVector;
+import progConc.MonitorAccionesWorker;
+import progConc.Operacion;
+import progConc.Worker;
 
 public class WorkerTest {
 
@@ -24,65 +27,10 @@ public class WorkerTest {
     private Worker worker2;
     private Buffer buffer;
     private Max operacionMax;
+    private double d;
 
 
- /*   private Worker worker1;
-    private Worker worker2;
-    private Worker worker3;
-    private Worker worker4;
-    private Buffer buffer;
-    private MonitorAccionesWorker unMonitor;
-    private Operacion operacionSum;
-    private Operacion operacionMul;
-    private Operacion operacionSet;
-    private Operacion operacionAbs;
-    private ConcurVector vector;
-    private ConcurVector vectorAux;
-    private int id1;
-    private int id2;
-    private int id3;
-    private int id4;
-    private int unInicio;
-    private int unFin;
-    private double nro;
 
-
-    @Before
-
-    public void setUp() {
-
-        this.buffer = new Buffer(5);
-        this.nro = 8.00;
-        this.unMonitor = new MonitorAccionesWorker();
-        this.operacionSum = new Add();
-        this.operacionMul = new Mul();
-        this.operacionAbs = new Abs();
-        this.operacionSet = new Set(nro);
-        this.vector = new ConcurVector(6, 3);
-        this.vector.setBuffer(buffer);
-        this.vectorAux = new ConcurVector(6, 3);
-        this.id1 = 1;
-        this.id2 = 2;
-        this.id3 = 3;
-        this.id4 = 4;
-        this.unInicio = 1;
-        this.unFin = 4;
-        this.worker1 = new Worker(unMonitor,operacionSum, vector, vectorAux, id1, unInicio, unFin );
-        this.worker2 = new Worker(unMonitor,operacionMul, vector, vectorAux, id2, unInicio, unFin );
-        this.worker3 = new Worker(unMonitor,operacionAbs, vector, vectorAux, id3, unInicio, unFin );
-        this.worker4 = new Worker(unMonitor,operacionSet, vector, vectorAux, id4, unInicio, unFin );
-
-    }
-
-    @Test
-    public void testeoSum(){
-
-        this.worker1.sum();
-
-        Assert.assertEquals(this.vector.norm(), 5);
-
-    }
-*/
 
     @Before
 
@@ -137,62 +85,13 @@ public class WorkerTest {
         Assert.assertEquals(expected, vector.max());
     }
 
+    @Test
+    public void testAbs() {
+        worker.abs();
+        // Verifica que worker escribiï¿½ en el buffer 2 veces
+        verify( mockVector,times(2)).getBuffer().write(d);
+
+    }
 
 
-=======
-package tests;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import progConc.ConcurVector;
-import progConc.MonitorAccionesWorker;
-import progConc.Operacion;
-import progConc.Worker;
-
-public class WorkerTest {
-	
-	
-	Operacion dummyOperacion;
-	MonitorAccionesWorker dummyMonitor;
-	Worker worker;
-	double d;
-	//Worker worker= new (workerthis.monitor, this.operacion,this.vector,this.vectorModificado,i,this.inicio,this.fin);
-	
-	ConcurVector mockVector;
-	Object dummy;  
-	
-		   
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		
-		dummyOperacion=mock(Operacion.class);
-		dummyMonitor= mock(MonitorAccionesWorker.class);
-		mockVector= mock (ConcurVector.class);
-		worker= new Worker (dummyMonitor,dummyOperacion,mockVector,mockVector,1,0,2);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testAbs() {
-		 worker.abs();
-		// Verifica que worker escribió en el buffer 2 veces
-		  verify( mockVector,times(2)).getBuffer().write(d);
-
-}
-	
->>>>>>> b96590bff97449adf17633596ba6c7140c80f2d7
 }
